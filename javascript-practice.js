b5838694464f9e13c87280b3e1998a817e86b325
Mike Selmani <mike_selmani@hotmail.com>
2022-07-07 20:52:32 -0400

    add the op module pattern calculator example

--- javascript-practice.js
+++ javascript-practice.js
@@ -24,3 +24,20 @@ function summ (a, b, c) {
 };
 
 console.log(summ(1, 2, 3));
+
+const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
+
+console.log(calculator.add(3,5)); // 8
+console.log(calculator.sub(6,2)); // 4
+console.log(calculator.mul(14,5534)); // 77476
